d2e82eb0d2184165a8cf48412eadfbcb59f3b2e1
Tarlan Huseynov <tarlan.huseynov@azintelecom.az>
2024-07-04 21:10:36 +0400

    init

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "downscaler_lambda_client" {
   source             = "./modules/downscaler"
-  eks_cluster_name   = "my-cluster"
+  eks_cluster_name   = var.cluster_name
   lambda_source      = "${path.root}/lambdas/downscaler"
   scale_out_schedule = "cron(00 09 ? * MON-FRI *)"
   scale_in_schedule  = "cron(00 18 ? * MON-FRI *)"
